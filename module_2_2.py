43b2b5b9e3c856780daf5fbe40c97ec2c549c9a5
Vitaliks <Naruto200@inbox.ru>
2024-08-03 00:11:00 +0500

    PyThon

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+print('Все ли равны?')
+first = int(input('first: '))
+second = int(input('second: '))
+third = int(input('third: '))
+
+if first == second == third:
+    print('3')
+elif first == second or first == third or second == third:
+    print('2')
+else:
+    print('0')
