7a12a14e6f792f9d6a747933877785d6ab1f827a
Vitaliks <Naruto200@inbox.ru>
2024-08-07 15:14:36 +0500

    PyThon

--- module_2_3.py
+++ module_2_3.py
@@ -1,26 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 f = 0
 while f < len(my_list):
-        if my_list[f] > 0:
-                print(my_list[f])
-                f = f + 1
-                continue
-        else:
-                my_list[f] = 0
-                f += 1
-                print(my_list[f])
-                break
+    if my_list[f] > 0:
+        print(my_list[f])
+        f = f + 1
+        continue
+    else:
+        my_list[f] = 0
+        f += 1
+        print(my_list[f])
+        break
 print('Список положительных чисел закончился')
-
-
-
-
-
-
-
-
-
-
-
-
-
