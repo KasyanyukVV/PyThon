59b35ca0d83d0c379ea8885741bfaa454d668a28
Vitaliks <Naruto200@inbox.ru>
2024-08-31 00:15:07 +0500

    PyThon

--- module_3_Hard.py
+++ module_3_Hard.py
@@ -1,61 +1,38 @@
-# def count_nums_and_strings(data):
-#     nums_sum = 0
-#     strings_len = 0
-#
-#     for item in data:
-#         if isinstance(item, int) or isinstance(item, float):
-#             nums_sum += item
-#         elif isinstance(item, str):
-#             strings_len += len(item)
-#         elif isinstance(item, (list, tuple)):
-#             sub_nums_sum, sub_strings_len = count_nums_and_strings(item)
-#             nums_sum += sub_nums_sum
-#             strings_len += sub_strings_len
-#         elif isinstance(item, dict):
-#             sub_nums_sum, sub_strings_len = count_nums_and_strings(list(item.values()))
-#             nums_sum += sub_nums_sum
-#             strings_len += sub_strings_len
-#
-#     return nums_sum, strings_len
-#
-#
-# data_structure = [
-#     [1, 2, 3],
-#     {'a': 4, 'b': 5},
-#     (6, {'cube': 7, 'drum': 8}),
-#     "Hello",
-#     ((), [{(2, 'Urban', ('Urban2', 35))}])
-# ]
-#
-# nums_sum, strings_len = count_nums_and_strings(data_structure)
-#
-# print("Сумма всех чисел:", nums_sum)
-# print("Суммарная длина всех строк:", strings_len)
 
-
-def calculate_sum(data):
+def calculate_structure_sum(data):
     total_sum = 0
 
-    for item in data:
-        if isinstance(item, int):
-            total_sum += item
-        elif isinstance(item, str):
-            total_sum += len(item)
-        elif isinstance(item, (list, tuple)):
-            total_sum += calculate_sum(item)
-        elif isinstance(item, dict):
-            total_sum += calculate_sum(list(item.values()))
+    # Если data - это список,кортэж,множества проходим по его элементам
+    if isinstance(data, (tuple, list, set)):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
 
+    # Если data - это словарь, проходим по его ключам и значениям
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            total_sum += calculate_structure_sum(key)
+            total_sum += calculate_structure_sum(value)
+
+    # Если data - это строка, добавляем её длину
+    elif isinstance(data, str):
+        total_sum += len(data)
+
+    # Если data - это число, добавляем его значение
+    elif isinstance(data, (int, float)):
+        total_sum += data
+
+    # Если data - это другой тип, игнорируем его
     return total_sum
 
 
+# Пример использования функции
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
     "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}] )
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-result = calculate_sum(data_structure)
+result = calculate_structure_sum(data_structure)
 print(result)
