f70fa00538a4f92768a4bde5536c69cced1193e3
Vitaliks <Naruto200@inbox.ru>
2024-10-03 18:24:38 +0500

    PyThon

--- module_6_1.py
+++ module_6_1.py
@@ -1,23 +1,9 @@
 class Animal:
     def __init__(self, name):
-        self.alive = True # живой
-        self.fed = False # накормленный
         self.name = name
+        self.alive = True       # (живой)
+        self.fed = False        # (накормленный)
 
-class Plant:
-    def __init__(self, name):
-        self.edible = False # съедобность
-        self.name = name
-
-class Mammal(Animal):
-    def eat (self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-class Predator(Animal):
     def eat(self, food):
         if food.edible:
             print(f"{self.name} съел {food.name}")
@@ -25,19 +11,40 @@ class Predator(Animal):
         else:
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
+
+
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False     # (съедобность)
+
+
+# Классы наследники
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
 class Flower(Plant):
     pass
+
 
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
         self.edible = True
 
+
+# Создание объектов классов
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
+# Выполнение действий
 print(a1.name)
 print(p1.name)
 
@@ -47,9 +54,5 @@ a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
-
-
-
-
 
 
