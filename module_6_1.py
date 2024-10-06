26e7113bd15ae392e8b69ff052ccda2bf1ca267a
Vitaliks <Naruto200@inbox.ru>
2024-10-06 20:17:43 +0500

    PyThon

--- module_6_1.py
+++ module_6_1.py
@@ -1,21 +1,22 @@
 class Animal:
+    alive = True  # (живой)         перенесен из фукции __init__
+    fed = False  # (накормленный)   перенесен из фукции __init__
     def __init__(self, name):
-        self.alive = True       # (живой)
-        self.fed = False        # (накормленный)
         self.name = name
 
     def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+        if isinstance(food, Plant): # добавлен isinstance к функции
+            if food.edible:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food.name}")
+                self.alive = False
 
 
 class Plant:
+    edible = False  # (съедобность) перенесен из фукции __init__
     def __init__(self, name):
-        self.edible = False     # (съедобность)
         self.name = name
 
 # Классы наследники
