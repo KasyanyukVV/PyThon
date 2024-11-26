ddfc6bbf3b962584fdd44b1c0aa24997d7908135
Vitaliks <Naruto200@inbox.ru>
2024-11-27 00:37:55 +0500

    PyThon

--- module_7_2.py
+++ module_7_2.py
@@ -1,31 +1,25 @@
 def custom_write(file_name, strings):
     strings_positions = {}
-
-    # Открываем файл для записи с кодировкой utf-8
     file = open(file_name, 'w', encoding='utf-8')
 
-    try:
-        for index, string in enumerate(strings, start=1):
-            byte_position = file.tell()  # Получаем текущую позицию в байтах
-            file.write(string + '\n')  # Записываем строку в файл с новой строки
-            strings_positions[(index, byte_position)] = string  # Сохраняем информацию в словарь
-    finally:
-        file.close()  # Закрываем файл
+    for index in range(len(strings)):
+        string = strings[index]
+        byte_position = file.tell()
+        file.write(string + '\n')
+        strings_positions[(index + 1, byte_position)] = string
 
+    file.close()
     return strings_positions
 
-# Пример использования
-if __name__ == "__main__":
-    info = [
-        'Text for tell.',
-        'Используйте кодировку utf-8.',
-        'Because there are 2 languages!',
-        'Спасибо!'
-    ]
-    result = custom_write('test.txt', info)
-
-    for elem in result.items():
-        print(elem)
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
 
 
 
